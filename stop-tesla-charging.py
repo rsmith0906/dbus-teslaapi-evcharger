b2eea391c5b58704dc87120689f3a91e07961ad0
Russell Smith (Valorem Consulting) <v-rusmit@microsoft.com>
2024-03-09 18:33:18 -0600

    Fixing log path

--- stop-tesla-charging.py
+++ stop-tesla-charging.py
@@ -49,7 +49,7 @@ class DbusTeslaAPIService:
                                 datefmt='%Y-%m-%d %H:%M:%S',
                                 level=logging.INFO,
                                 handlers=[
-                                    logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
+                                    logging.FileHandler("%s/current-stop-charging.log" % (os.path.dirname(os.path.realpath(__file__)))),
                                     logging.StreamHandler()
                                 ])
 
